029fdaa09edf8722d48ee3aa7aa92479f76343a4
sammymax <philwsun@gmail.com>
2019-07-07 16:50:31 -0400

    Sound selection thing now shows items

--- app/src/main/java/me/psun/sunrise/SunriseFragment.kt
+++ app/src/main/java/me/psun/sunrise/SunriseFragment.kt
@@ -5,17 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.ArrayAdapter
+
+
 
 class SunriseFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var spinner : Spinner? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.mode_sunrise, container, false)
+        val view = inflater.inflate(R.layout.mode_sunrise, container, false)
+        spinner = view.findViewById(R.id.alarm_spinner)
+        val spinnerArray = listOf("Martin Garrix - Poision", "DVLM & Martin Garrix - Tremor", "Sheck Wes - Mo Bamba", "Swedish House Mafia & Knife Party - Antidote")
+        context?.let {
+            val adapter = ArrayAdapter<String>(
+                it, android.R.layout.simple_spinner_item, spinnerArray
+            )
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner?.adapter = adapter
+        }
+        return view
     }
 }

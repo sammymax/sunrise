a8ffa144d1d4f1e9ef9e27a95b3f667255e65771
sammymax <philwsun@gmail.com>
2019-07-07 22:44:13 -0400

    Alarm is called now

---
+++ app/src/main/java/me/psun/sunrise/RingingAlarm.kt
@@ -0,0 +1,12 @@
+package me.psun.sunrise
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class RingingAlarm : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        Log.e("asd", "yooo")
+    }
+}
--- app/src/main/java/me/psun/sunrise/SunriseFragment.kt
+++ app/src/main/java/me/psun/sunrise/SunriseFragment.kt
@@ -1,17 +1,26 @@
 package me.psun.sunrise
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Spinner
 import android.widget.ArrayAdapter
-
+import android.widget.TimePicker
+import java.util.*
+import android.content.Context.ALARM_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 
 
 class SunriseFragment : Fragment() {
     private var spinner : Spinner? = null
+    private var timePicker : TimePicker? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +28,26 @@ class SunriseFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.mode_sunrise, container, false)
         spinner = view.findViewById(R.id.alarm_spinner)
+        timePicker = view.findViewById(R.id.timePicker)
+
+        timePicker?.setOnTimeChangedListener{_, hour, minute ->
+            Log.e("times", "yoooo $hour:$minute")
+            val c = GregorianCalendar()
+            Log.e("times", "yooo2 ${c.get(GregorianCalendar.HOUR_OF_DAY)}:${c.get(GregorianCalendar.MINUTE)}")
+            Log.e("times", c.timeInMillis.toString())
+            if (c.get(GregorianCalendar.HOUR_OF_DAY) > hour || (c.get(GregorianCalendar.HOUR_OF_DAY) == hour && c.get(GregorianCalendar.MINUTE) >= minute))
+                c.add(GregorianCalendar.DATE, 1)
+            c.set(GregorianCalendar.HOUR_OF_DAY, hour)
+            c.set(GregorianCalendar.MINUTE, minute)
+            c.set(GregorianCalendar.SECOND, 0)
+            c.set(GregorianCalendar.MILLISECOND, 0)
+            Log.e("times", c.timeInMillis.toString())
+
+            val alarm = activity!!.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, Intent(context, RingingAlarm::class.java), 0)
+            alarm.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent)
+        }
+
         val spinnerArray = listOf("Martin Garrix - Poision", "DVLM & Martin Garrix - Tremor", "Sheck Wes - Mo Bamba", "Swedish House Mafia & Knife Party - Antidote")
         context?.let {
             val adapter = ArrayAdapter<String>(

e6f9cd2e0e65dc43191ab683daabcd9ff53dfbfe
sammymax <philwsun@gmail.com>
2019-07-08 22:06:38 -0400

    Floating action buttons and stuff...now mostly functional

--- app/src/main/java/me/psun/sunrise/NoAlarmView.kt
+++ app/src/main/java/me/psun/sunrise/NoAlarmView.kt
@@ -3,6 +3,7 @@ package me.psun.sunrise
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NoAlarmView : FrameLayout {
     constructor (context: Context) : super(context)
@@ -10,7 +11,14 @@ class NoAlarmView : FrameLayout {
     constructor (context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle)
     constructor (context: Context, attrs: AttributeSet?, defStyle: Int, defStyleRes: Int) : super(context, attrs, defStyle, defStyleRes)
 
+    var addAlarm : FloatingActionButton? = null;
+
     init {
         inflate(context, R.layout.no_alarm_view, this)
+        addAlarm = findViewById(R.id.no_alarm_plus)
+    }
+
+    fun setAddAlarmFunc(listener : OnClickListener) {
+        addAlarm?.setOnClickListener(listener)
     }
 }
--- app/src/main/java/me/psun/sunrise/SunriseFragment.kt
+++ app/src/main/java/me/psun/sunrise/SunriseFragment.kt
@@ -6,24 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SunriseFragment(val appState: AppState) : Fragment() {
     private var alarmTimeShower : TextView? = null
     private var alarmAMPM : TextView? = null
-    private var editButton : Button? = null
+    private var editButton : FloatingActionButton? = null
     private var tilSunrise : TextView? = null
+    private var noAlarmOverlay : NoAlarmView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.mode_sunrise, container, false)
-        editButton = view.findViewById(R.id.edit_alarm)
+        editButton = view.findViewById(R.id.alarm_edit)
         alarmTimeShower = view.findViewById(R.id.alarm_time)
         alarmAMPM = view.findViewById(R.id.alarm_am_pm)
         tilSunrise = view.findViewById(R.id.til_sunrise)
+        noAlarmOverlay = view.findViewById(R.id.no_alarm_overlay)
 
-        editButton?.setOnClickListener{_ ->
+        val showDialog = View.OnClickListener{_ ->
             val dialog = AlarmDialogFragment()
             dialog.setAlarmListener(object : AlarmListener{
                 override fun onChange(hour: Int, minute: Int, soundId: Int?) {
@@ -41,11 +44,13 @@ class SunriseFragment(val appState: AppState) : Fragment() {
                     val hoursLeft = millisLeft / (60 * 60 * 1000)
                     val minutesLeft = (millisLeft - 60 * 60 * 1000 * hoursLeft) / (60 * 1000)
                     tilSunrise?.text = "${hoursLeft} hours, ${minutesLeft} minutes until sunrise"
+                    noAlarmOverlay?.visibility = View.INVISIBLE
                 }
             })
             dialog.show(fragmentManager!!, "alarmDialog")
         }
-
+        editButton?.setOnClickListener(showDialog)
+        noAlarmOverlay?.setAddAlarmFunc(showDialog)
 
         return view
     }

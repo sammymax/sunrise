09db2052fde73f09be833224b5922517d21ab62f
sammymax <philwsun@gmail.com>
2019-07-09 17:09:58 -0400

    Major aesthetic upgrade, especially to alarm ringing screen

--- app/src/main/java/me/psun/sunrise/RingingAlarmActivity.kt
+++ app/src/main/java/me/psun/sunrise/RingingAlarmActivity.kt
@@ -1,20 +1,22 @@
 package me.psun.sunrise
 
+import android.animation.AnimatorInflater
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
 import android.os.PowerManager
+import android.widget.TableLayout
+import android.widget.TableRow
+import kotlin.random.Random
 
 class RingingAlarmActivity : Activity() {
-    var off : Button? = null
     var mediaPlayer : MediaPlayer? = null
     var wakelock : PowerManager.WakeLock? = null
 
@@ -48,6 +50,45 @@ class RingingAlarmActivity : Activity() {
         }
         setContentView(R.layout.ringing_alarm)
 
+        val grid = findViewById<TableLayout>(R.id.ringing_grid)
+        val numButtons = grid.childCount * (grid.getChildAt(0) as TableRow).childCount
+        val numSnooze = numButtons / 2
+        val idxToSnooze = (0 until numButtons).shuffled().take(numSnooze).toSet()
+        var idx = 0
+        for (i in 0 until grid.childCount) {
+            val row = grid.getChildAt(i) as TableRow
+            for (j in 0 until row.childCount) {
+                val button = row.getChildAt(j) as Button
+                if (idx in idxToSnooze) {
+                    button.text = "Snooze"
+                    button.setBackgroundColor(0xFFFFFFFF.toInt())
+                    button.setTextColor(0xFF000000.toInt())
+                    button.setOnClickListener{_ ->
+                        stopAlarm(true)
+                    }
+                    AnimatorInflater.loadAnimator(this, R.animator.alarm_flash_snooze).apply{
+                        startDelay = Random.nextInt(2000).toLong()
+                        duration = Random.nextInt(1800, 2800).toLong()
+                        setTarget(button)
+                        start()
+                    }
+                } else {
+                    button.text = "Dismiss"
+                    button.setBackgroundColor(0xFF1A9185.toInt())
+                    button.setOnClickListener{_ ->
+                        stopAlarm(false)
+                    }
+                    AnimatorInflater.loadAnimator(this, R.animator.alarm_flash_dismiss).apply{
+                        startDelay = Random.nextInt(2000).toLong()
+                        duration = Random.nextInt(1800, 2800).toLong()
+                        setTarget(button)
+                        start()
+                    }
+                }
+                idx++
+            }
+        }
+
         val pm = applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
         wakelock = pm.newWakeLock(
             PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
@@ -55,11 +96,6 @@ class RingingAlarmActivity : Activity() {
         )
         wakelock?.acquire()
         mediaPlayer?.start()
-
-        off = findViewById(R.id.alarm_off)
-        off?.setOnClickListener{_ ->
-            stopAlarm(false)
-        }
     }
 
     fun stopAlarm (snooze: Boolean) {

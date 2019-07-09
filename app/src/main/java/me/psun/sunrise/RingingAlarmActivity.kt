86afdd04b89eb31401f3d8a5d0fdb84e73edae4f
sammymax <philwsun@gmail.com>
2019-07-09 10:02:22 -0400

    RingingAlarmActivity cleanup; window flags fixed so that navigation bar stays hidden even after press, and release wake lock after use

--- app/src/main/java/me/psun/sunrise/RingingAlarmActivity.kt
+++ app/src/main/java/me/psun/sunrise/RingingAlarmActivity.kt
@@ -15,28 +15,28 @@ import android.os.PowerManager
 class RingingAlarmActivity : Activity() {
     var off : Button? = null
     var mediaPlayer : MediaPlayer? = null
+    var wakelock : PowerManager.WakeLock? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("ad", "ringing toyoooo")
-        val songId = intent.getIntExtra("SongIdentifier", -1)
-        if (songId >= 0) {
+        val songId = intent.getIntExtra("SongIdentifier", AppState.NO_SOUND_ID)
+        if (songId != AppState.NO_SOUND_ID) {
             mediaPlayer = MediaPlayer.create(this, songId)
             mediaPlayer?.isLooping = true
         }
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        //window.addFlags(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY)
-        //window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL)
-        //window.addFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
-        //window.addFlags(WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         window.decorView.apply {
-            // Hide both the navigation bar and the status bar.
-            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-            // a general rule, you should design your app to hide the status bar whenever you
-            // hide the navigation bar.
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or
+                    // layout stuff
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    // actual look
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setTurnScreenOn(true)
@@ -48,20 +48,25 @@ class RingingAlarmActivity : Activity() {
         setContentView(R.layout.ringing_alarm)
 
         val pm = applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
-        val wakeLock = pm.newWakeLock(
-            PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+        wakelock = pm.newWakeLock(
+            PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
             "sunrise:Alarm"
         )
-        wakeLock.acquire()
+        wakelock?.acquire()
         mediaPlayer?.start()
 
         off = findViewById(R.id.alarm_off)
         off?.setOnClickListener{_ ->
-            Log.e("pressed", "pressed")
-            mediaPlayer?.stop()
-            mediaPlayer?.release()
-            mediaPlayer = null
-            finish()
+            stopAlarm()
         }
+    }
+
+    fun stopAlarm () {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+        wakelock?.release()
+        wakelock = null
+        finish()
     }
 }

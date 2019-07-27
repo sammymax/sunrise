47cfa22b5f66a4bc68580d93a6df9e4234ed63aa
sammymax <philwsun@gmail.com>
2019-07-27 13:05:27 -0400

    Send UDP packet immediately for BPM mode
    Fix bug that caused crash after extensive number of packets (socket must be closed?)

--- app/src/main/java/me/psun/sunrise/colorio/H801ColorListener.kt
+++ app/src/main/java/me/psun/sunrise/colorio/H801ColorListener.kt
@@ -17,39 +17,27 @@ class H801ColorListener(val getMAC: () -> String) : ColorListener {
 
     override fun setRGB(rgb: Int, source: AppState.ColorSetSource) {
         this.rgb = rgb
-        scheduleUpdate()
+        if (source == AppState.ColorSetSource.BPM) executeNow(3)
+        else scheduleUpdate()
     }
 
     override fun setCW(cw: Int, source: AppState.ColorSetSource) {
         this.cw = cw
-        scheduleUpdate()
+        if (source == AppState.ColorSetSource.BPM) executeNow(3)
+        else scheduleUpdate()
     }
 
     override fun setWW(ww: Int, source: AppState.ColorSetSource) {
         this.ww = ww
-        scheduleUpdate()
+        if (source == AppState.ColorSetSource.BPM) executeNow(3)
+        else scheduleUpdate()
     }
 
     private fun scheduleUpdate() {
         var delay = 250L
         while (numScheduled < 5) {
             handler.postDelayed({
-                val mac = getMAC()
-                val buf = ByteArray(11)
-                buf[0] = hexStringToByte("FB")
-                buf[1] = hexStringToByte("EB")
-                buf[2] = (rgb shr 16).toByte()
-                buf[3] = ((rgb shr 8) and 255).toByte()
-                buf[4] = (rgb and 255).toByte()
-                buf[5] = ww.toByte()
-                buf[6] = cw.toByte()
-                // 7, 8, 9 are from MAC: 6th, 5th, 4th octets respectively
-                buf[7] = hexStringToByte(mac.substring(10, 12))
-                buf[8] = hexStringToByte(mac.substring(8, 10))
-                buf[9] = hexStringToByte(mac.substring(6, 8))
-                buf[10] = 0
-
-                UpdateTask().execute(buf)
+                executeNow()
                 numScheduled--
             }, delay)
             delay -= 50
@@ -57,11 +45,37 @@ class H801ColorListener(val getMAC: () -> String) : ColorListener {
         }
     }
 
+    private fun executeNow(count: Int = 1) {
+        val mac = getMAC()
+        val buf = ByteArray(11)
+        buf[0] = hexStringToByte("FB")
+        buf[1] = hexStringToByte("EB")
+        buf[2] = (rgb shr 16).toByte()
+        buf[3] = ((rgb shr 8) and 255).toByte()
+        buf[4] = (rgb and 255).toByte()
+        buf[5] = ww.toByte()
+        buf[6] = cw.toByte()
+        // 7, 8, 9 are from MAC: 6th, 5th, 4th octets respectively
+        buf[7] = hexStringToByte(mac.substring(10, 12))
+        buf[8] = hexStringToByte(mac.substring(8, 10))
+        buf[9] = hexStringToByte(mac.substring(6, 8))
+        buf[10] = 0
+
+        for (z in 1..count)
+            UpdateTask().execute(buf)
+    }
+
     private class UpdateTask : AsyncTask<ByteArray, Unit, Unit>() {
         override fun doInBackground(vararg p0: ByteArray) {
             val h801Addr = InetAddress.getByName("192.168.4.1")
             val packet = DatagramPacket(p0[0], p0[0].size, h801Addr, 30977)
-            DatagramSocket().send(packet)
+            val socket = DatagramSocket()
+            try {
+                socket.send(packet)
+            } catch (e: Exception) {}
+            finally {
+                socket.close()
+            }
         }
     }
 
